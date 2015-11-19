83d27e8c56e2fc3771556a128c2d2c98c657663b
Larry <tradesouthwest@outlook.com>
2015-11-19 11:43:28 -0700

    clean up
    spacing

--- footer.php
+++ footer.php
@@ -2,6 +2,7 @@
 <footer class="footer">
     <div class="container-fluid">
         <div class="row">
+        	
             <div class="col-lg-6 col-xs-6">
                 <p class="copyright">&copy; <?php esc( $det_name ); ?></p>
                 <p><a class="btn btn-link" href="<?php esc( $theme_url ); ?>/admin/index.php" title="lists">Config</a></p>
@@ -15,6 +16,7 @@
                 </ul>
                 </form>
             </div>
+            
         </div>
     </div>
 </footer>
@@ -23,6 +25,5 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-	
 </body>
 </html>
