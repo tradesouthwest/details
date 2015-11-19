462add2c33ef2086fc451619a16cc5945341f9bd
Larry <tradesouthwest@outlook.com>
2015-11-19 11:42:00 -0700

    whitespace
    line 12, 18, 19

--- nav.php
+++ nav.php
@@ -9,17 +9,18 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="<?php esc( $theme_url ); ?>/index.php"><img src="<?php esc( $theme_url ); ?>/lib/img/tsw-logoem.png" /></a>
+            <a class="navbar-brand" href="<?php esc( $theme_url ); ?>/index.php">
+                <img src="<?php esc( $theme_url ); ?>/lib/img/tsw-logoem.png" />
+            </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav" id="grad-menu">
-                <li><a class="grad-anchor" href="<?php esc( $theme_url ); ?>/index.php"       title="forms">Read Articles</a></li>
-                <li><a class="grad-anchor" href="<?php esc( $theme_url ); ?>/view-list.php"    title="forms">List Articles</a></li>
+                <li><a class="grad-anchor" href="<?php esc( $theme_url ); ?>/index.php"    title="forms">Read Articles</a></li>
+                <li><a class="grad-anchor" href="<?php esc( $theme_url ); ?>/view-list.php" title="forms">List Articles</a></li>
             </ul>
             <h1 id="nav-header-title" class="nopadding"><?php esc( $det_name ); ?></h1>
         </div>
     </div>
-            
 </nav>
     </div><!--/.nav-collapse -->
 </div><!--/.container-fluid -->
